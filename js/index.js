dec7b2e857398ffbe104cc1f8a3b79f3337b6a2c
suvra2001 <112110876+suvra2001@users.noreply.github.com>
2023-02-26 14:48:09 -0500

    idek

--- js/index.js
+++ js/index.js
@@ -11,3 +11,49 @@ function openTab(evt, tabName) {
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
   }
+
+  function winLoss (){
+    const brush = vl.selectInterval().encodings("x");
+      
+    const wins = vl.markBar()
+      .data(nba_elo_typed)
+      .transform(
+        vl.filter(brush),
+        vl.calculate('datum.game_result == "W" ? "Wins" : "Losses"').as('Outcome'),
+        vl.aggregate(vl.count().as('count')).groupby('fran_id', 'Outcome'),
+      )
+      .title("Win and Loss Count by Team")
+      .encode(
+        vl.x().fieldN('fran_id').axis({title: 'Team'}),
+        vl.y().fieldQ('count'),
+        vl.color().fieldN('Outcome').scale({ 
+            range: ["red", "green"],
+          }),
+        vl.tooltip(['fran_id', 'Outcome', 'count']),
+        );
+    
+    const timeplot =  vl.markLine({
+      point: {
+          size: 50,
+          filled: true,
+          stroke: "white"
+        }
+      })
+      .title("Maximum ELO of Each Year")
+      .select(brush)
+      .data(nba_elo_typed)
+      .transform(
+        vl.groupby('year_id').aggregate(vl.max('elo_n').as('Max ELO')),
+      )
+      .encode(
+          vl.x().fieldN("year_id").axis({title: 'Year'}),
+          vl.y().fieldQ('Max ELO').scale({domain: [1600, 1900]}),
+          vl.color().value('purple'),
+          vl.tooltip(['Max ELO', 'fran_id']),
+        );
+    
+      return vl.vconcat(wins, timeplot)
+        .spacing(20)
+        .render();
+    }
+    
